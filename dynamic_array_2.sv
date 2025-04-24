62a8556bc2e149587215888bb9a9106907a23497
Vishnu Deep PandurangaRao <143198224+vdspw@users.noreply.github.com>
2025-04-24 06:04:21 -0700

    Create dynamic_array_2.sv

---
+++ dynamic_array_2.sv
@@ -0,0 +1,17 @@
+module tb;
+
+int arr[];
+
+initial begin
+  arr =  new[5];
+  
+  for(int i=0;i<5;i++)begin
+    arr[i] =5 *1;
+  end
+  
+  $display("arr: %0p",arr);
+  
+  arr = new[30]; //automatically removes the previous elements.
+  $display("arr: %0p",arr);
+  
+end
