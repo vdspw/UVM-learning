23a8fba2752c17f38a346d23da11ac0e0a56ef2c
Vishnu Deep PandurangaRao <143198224+vdspw@users.noreply.github.com>
2025-07-13 18:32:15 -0700

    Update multiply_function.sv
    comparision between normal computing and function , for multiplication

--- multiply_function.sv
+++ multiply_function.sv
@@ -3,17 +3,21 @@ module tb;
   reg[15:0] result=0;
   reg[4:0] ain = 4'b0100;
   reg[4:0] bin = 4'b0011;
+  reg[15:0] normal;
   
   function bit[15:0] mul();
     return ain*bin;
+    
+    
   endfunction
   
     
   initial begin
+    normal = ain*bin;
     result = mul();
-    
-    $display("Value of addition : %0d", result);
-    if (result == 12) $display("TEST PASSED");
+    $display("Value of mul : %0d", normal);
+    $display("Value of mul : %0d", result);
+    if (result == normal) $display("TEST PASSED");
     else $display ("TEST FAILED");
   end
   
