a00727c5bca706a801d7edc96757810d495c6f02
Vishnu Deep PandurangaRao <143198224+vdspw@users.noreply.github.com>
2025-07-24 05:29:14 -0700

    Create rand_c.sv
    demo of a cyclic rand -- numbers generated are not repeated.

---
+++ rand_c.sv
@@ -0,0 +1,20 @@
+class generator;
+  randc bit [3:0] a,b; //eq ports for input and output
+  bit [3:0] y;
+  
+  
+endclass
+
+module tb;
+  
+  generator gen;
+  int i;
+  initial begin
+  gen =  new();
+  for(i=0; i <10; i++) begin
+    gen.randomize();
+    $display(" Value of a: %0d ----- Value of b : %0d ", gen.a,gen.b);
+    #10;
+  end
+  end
+endmodule
